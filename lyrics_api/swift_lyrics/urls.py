07d2a127a79da186fc8a43d3c4efc8f01b3a6752
Reiner Marquez <rmaceissoft@gmail.com>
2021-05-29 21:24:33 -0500

    simplied the code by using viewsets instead of generic views

--- lyrics_api/swift_lyrics/urls.py
+++ lyrics_api/swift_lyrics/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls import url
+from django.urls import include
 
 from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from swift_lyrics import views
+
+
+router = DefaultRouter()
+router.register(r"album", views.AlbumViewSet, basename="album")
+router.register(r"song", views.SongViewSet, basename="song")
+router.register(r"lyric", views.LyricViewSet, basename="lyric")
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -19,13 +28,9 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+
 urlpatterns = [
-    url(r'^lyric/$', views.APIIndex.as_view(), name='api_index'),
-    url(r'^lyric/(?P<pk>\d+)/?$', views.APIDetail.as_view(), name='api_detail'),
-    url(r'^album/$', views.AlbumIndex.as_view(), name='album_index'),
-    url(r'^album/(?P<pk>\d+)/?$', views.AlbumDetail.as_view(), name='album_detail'),
-    url(r'^song/$', views.SongIndex.as_view(), name='song_index'),
-    url(r'^song/(?P<pk>\d+)/?$', views.SongDetail.as_view(), name='song_detail'),
+    url(r'^', include(router.urls)),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- lyrics_api/swift_lyrics/views.py
+++ lyrics_api/swift_lyrics/views.py
@@ -1,13 +1,12 @@
-from rest_framework import mixins, generics, filters, status
+from rest_framework import mixins, filters, status, viewsets
 from rest_framework.response import Response
 
 from django.http import HttpResponse
 from django.views import View
 # Create your views here.
 from swift_lyrics.models import Lyric, Album, Song
-from swift_lyrics.serializers.serializer import LyricSerializer, BaseSongSerializer, BaseAlbumSerializer, \
-    AlbumDetailSerializer, \
-    SongDetailSerializer, LyricSerializer, SongSerializer, LyricDetailSerializer
+from swift_lyrics.serializers.serializer import BaseAlbumSerializer, \
+    AlbumDetailSerializer, SongDetailSerializer, SongSerializer, LyricDetailSerializer
 
 
 class HealthCheckView(View):
@@ -18,88 +17,35 @@ class HealthCheckView(View):
         return HttpResponse("ok")
 
 
-class AlbumIndex(mixins.ListModelMixin,
-                 generics.GenericAPIView):
-    serializer_class = BaseAlbumSerializer
+class AlbumViewSet(mixins.ListModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = Album.objects.all()
 
-    def get_queryset(self):
-        return Album.objects.all()
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return AlbumDetailSerializer
+        else:
+            return BaseAlbumSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-
-class AlbumDetail(mixins.RetrieveModelMixin,
+class SongViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
                   mixins.DestroyModelMixin,
-                generics.GenericAPIView):
-    serializer_class = AlbumDetailSerializer
+                  viewsets.GenericViewSet):
+    queryset = Song.objects.all()
 
-    def get_queryset(self):
-        return Album.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return SongDetailSerializer
+        else:
+            return SongSerializer
 
 
-class SongIndex(mixins.ListModelMixin,
-                 generics.GenericAPIView):
-    serializer_class = SongSerializer
-
-    def get_queryset(self):
-        return Song.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-
-class SongDetail(mixins.RetrieveModelMixin,
-                 mixins.DestroyModelMixin,
-                generics.GenericAPIView):
-    serializer_class = SongDetailSerializer
-
-    def get_queryset(self):
-        return Song.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class APIIndex(mixins.ListModelMixin,
-               mixins.CreateModelMixin,
-               generics.GenericAPIView):
+class LyricViewSet(viewsets.ModelViewSet):
+    queryset = Lyric.objects.all()
     serializer_class = LyricDetailSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['text', 'song__name', 'song__album__name']
     ordering_fields = ['text', 'song__name', 'song__album__name']
-
-
-    def get_queryset(self):
-        return Lyric.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class APIDetail(mixins.RetrieveModelMixin,
-                mixins.UpdateModelMixin,
-                mixins.DestroyModelMixin,
-                generics.GenericAPIView):
-    serializer_class = LyricDetailSerializer
-
-    def get_queryset(self):
-        return Lyric.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
