2a8592b211f2c3c3d07afea5001fa0c261d3a948
Reiner Marquez <rmaceissoft@gmail.com>
2021-05-30 21:26:18 -0500

    cleanup

--- lyrics_api/swift_lyrics/views.py
+++ lyrics_api/swift_lyrics/views.py
@@ -1,4 +1,4 @@
-from rest_framework import mixins, filters, status, viewsets
+from rest_framework import mixins, filters, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -8,7 +8,7 @@ from django.views import View
 from django_filters import rest_framework as dj_filters
 from drf_yasg import openapi
 from drf_yasg.utils import no_body, swagger_auto_schema
-# Create your views here.
+
 from swift_lyrics.models import Artist, Lyric, Album, Song
 from swift_lyrics.serializers.serializer import BaseAlbumSerializer, \
     AlbumDetailSerializer, AlbumWriteSerializer, SongDetailSerializer, \
