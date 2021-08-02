b10876dd3fc54f3002adc60008ff60e41a2d0b96
Jose Carlos Delgado Ramos <jcdelgadoramos@outlook.com>
2021-08-02 14:21:07 -0500

    #2 models changes in artist and related

--- lyrics_api/swift_lyrics/models.py
+++ lyrics_api/swift_lyrics/models.py
@@ -1,12 +1,35 @@
 from django.db import models
 
 
+class Artist(models.Model):
+    name = models.TextField(
+        blank=False,
+        db_index=True,
+        unique=True,
+        help_text="Artist name - can alternatively use 'id' field set to id of existing artist when creating new lyrics")
+    first_year_active = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True)
+
+    objects = models.Manager()
+
+
 class Album(models.Model):
     name = models.TextField(
         blank=False,
         db_index=True,
         unique=True,
         help_text="Album name - can alternatively use 'id' field set to id of existing album when creating new lyrics")
+    year = models.PositiveSmallIntegerField(
+        blank=False,
+        null=True)
+    artist = models.ForeignKey(
+        Artist,
+        related_name="albums",
+        null=True,
+        blank=False,
+        on_delete=models.CASCADE,
+        help_text="Artist")
 
     objects = models.Manager()
 
